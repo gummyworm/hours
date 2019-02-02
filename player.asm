24a9705fc5a55d48424a03927a620b2962aa5475
Bryce Wilson <brycemwilson@gmail.com>
2019-02-02 14:17:00 -0500

    Cleanup

--- player.asm
+++ player.asm
@@ -23,74 +23,82 @@ dir: .byte 0
 	stx prevx
 	sty prevy
 
-	jsr joy::up
-	bne :+
 @up:
+	jsr joy::up
+	bne @down
 	dec ypos
 	inc @dirty
-:	jsr joy::down
-	bne :+
-@down:
+	lda #DIR_UP
+	sta dir
+
+@down:	jsr joy::down
+	bne @left
 	inc ypos
 	inc @dirty
-:	jsr joy::left
-	bne :+
-@left:
-.ifdef MULTICOLOR
-	dec xpos
-.endif
-	dec xpos
-	inc @dirty
-:	jsr joy::right
-	bne :+
-@right:
-.ifdef MULTICOLOR
-	inc xpos
-.endif
-	inc xpos
-	inc @dirty
-:	jsr joy::fire
-	bne :+
-@fire:
+	lda #DIR_DOWN
+	sta dir
 
-:	lda @dirty
+@left:	jsr joy::left
+	bne @right
+.ifdef MULTICOLOR
+	dec xpos
+.endif
+	dec xpos
+	inc @dirty
+	lda #DIR_LEFT
+	sta dir
+
+@right: jsr joy::right
+	bne @fire
+.ifdef MULTICOLOR
+	inc xpos
+.endif
+	inc xpos
+	inc @dirty
+	lda #DIR_RIGHT
+	sta dir
+
+@fire:	jsr joy::fire
+	bne @inputdone
+
+@inputdone:
+	lda @dirty
 	beq @done
 
 	ldx prevx
 	ldy prevy
 	jsr sprite::off
 
+@screenright:
 	lda xpos
 	cmp #SCREEN_W*8
-	bne :+
-@screenright:
+	bne @screenleft
 	lda #$00
 	sta xpos
 	jsr gen::scrolll
 	jmp @updateplayer
 
-:
+@screenleft:
 .ifdef MULTICOLOR
 	cmp #$fe
 .else
 	cmp #$ff
 .endif
-	bne :+
-@screenleft:
+	bne @screendown
 	lda #SCREEN_W*8-8
 	sta xpos
 	jsr gen::scrollr
 	jmp @updateplayer
 
-:	lda ypos
-	cmp #SCREEN_H*8-8
-	bne :+
 @screendown:
+	lda ypos
+	cmp #SCREEN_H*8-8
+	bne @screenup
 	lda #0
 	sta ypos
 	jsr gen::scrollu
 	jmp @redrawplayer
-:
+
 @screenup:
 	cmp #$ff
 	bne @updateplayer
@@ -104,20 +112,18 @@ dir: .byte 0
 	ldx xpos
 	ldy ypos
 	jsr canmove
-	bne @stay
+	beq @redrawplayer
+@stay:	ldx prevx
+	ldy prevy
+	stx xpos
+	sty ypos
+
 @redrawplayer:
 	ldx xpos
 	ldy ypos
 	lda #PLAYER
 	jsr sprite::on
 @done:	rts
-@stay:	ldx prevx
-	ldy prevy
-	stx xpos
-	sty ypos
-	lda #PLAYER
-	jsr sprite::on
-	rts
 
 prevx: .byte 0
 prevy: .byte 0
