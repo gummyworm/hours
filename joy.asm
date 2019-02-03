3823e861fe0f03f50fd51c5a896f505bab7491bf
Bryce Wilson <brycemwilson@gmail.com>
2019-02-03 16:10:48 -0500

    Fix docs

--- joy.asm
+++ joy.asm
@@ -1,6 +1,13 @@
 .CODE
-;--------------------------------------
+
 .export __joy_init
+.export __joy_fire
+.export __joy_up
+.export __joy_down
+.export __joy_left
+.export __joy_right
+
+;--------------------------------------
 .proc __joy_init
         lda #$00
         sta $9113       ;set DDR for VIA #1 to input for joystick
@@ -9,27 +16,27 @@
         rts
 .endproc
 
-.export __joy_fire
-.export __joy_up
-.export __joy_down
-.export __joy_left
-.export __joy_right
-; .Z is set if Down is pressed
+;--------------------------------------
+; up returns .Z set if UP is pressed
 __joy_up:
         lda #$04
 	.byte $2c
+; down returns .Z set if DOWN is pressed
 __joy_down:
 	lda #$08
 	.byte $2c
+; left returns .Z set if LEFT is pressed
 __joy_left:
 	lda #$10
 	.byte $2c
+; fire returns .Z set if FIRE is pressed
 __joy_fire:
 	lda #$20
 	and $9111
 	rts
 
-; .Z is set if Down is pressed
+;--------------------------------------
+; right returns .Z set if right is pressed
 .export __joy_right
 __joy_right:
 	lda $9120
